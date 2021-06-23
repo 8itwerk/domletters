32d945aa6f3d701e908ccc36ceb654e65e4a1f50
Jeffrey Jernstrom <smidt@pdx.edu>
2021-06-23 16:57:47 -0700

    Created a basic Makefile and domletters.cpp to get going.

---
+++ Makefile
@@ -0,0 +1,4 @@
+CFLAGS= -g -Wall
+
+domletters:
+	g++ domletters.cpp -o domletters $(CFLAGS) 
---
+++ domletters.cpp
@@ -0,0 +1,14 @@
+// Jeffrey Jernstrom
+// CS4
+// Homework 1
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  cout << endl << "Hello World!" << endl;
+
+  return 0;
+}
