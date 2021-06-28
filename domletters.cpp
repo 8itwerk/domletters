12f0c222f610dc05a1d7a718e9660bd28d38f1e9
Jeffrey Jernstrom <smidt@pdx.edu>
2021-06-28 16:36:13 -0700

    Main now takes input word by word and created a seporate function to process words

--- domletters.cpp
+++ domletters.cpp
@@ -2,13 +2,42 @@
 // CS4
 // Homework 1
 
+#define SIZE 100
+
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+int count_dom(const char * word);
 
 int main() {
-  cout << endl << "Hello World!" << endl;
+
+  int total = 0;
+  char word[SIZE];
+
+  // Check for input
+
+  // Read in all of the words separated by white space until the eof
+  while (cin.peek() != EOF) {
+    cin >> word;
+    // Process word
+    total += count_dom(word);
+  }
 
   return 0;
+}
+
+
+
+int count_dom(const char * word) {
+  int count = 0;
+  
+  // Count the number of dom letters
+  for (int i = 0; i != '\0' && i < SIZE; ++i) {
+    // Check for only alphabetic words
+    if (word[i] > 'z' || word[i] < 'A')
+      return 0;
+  }
+
+  return count;
 }
